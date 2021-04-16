41e82a71d985e45e9f63a63ca755f0a2f7b8a8a9
manuel <manuel.bettin@studenti.unipd.it>
2021-04-16 22:47:45 +0200

    update with new servlets

--- 4doctors/src/main/java/it/unipd/dei/webapp/servlet/PrescriptionRequestServlet.java
+++ 4doctors/src/main/java/it/unipd/dei/webapp/servlet/PrescriptionRequestServlet.java
@@ -1,6 +1,8 @@
 package it.unipd.dei.webapp.servlet;
 
+import it.unipd.dei.webapp.dao.ListMedicinesDAO;
 import it.unipd.dei.webapp.dao.PrescriptionRequestDAO;
+import it.unipd.dei.webapp.resource.Medicine;
 import it.unipd.dei.webapp.resource.Message;
 import it.unipd.dei.webapp.resource.Prescription;
 import it.unipd.dei.webapp.utils.InputFormatException;
@@ -12,6 +14,7 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.Date;
 import java.sql.SQLException;
+import java.util.*;
 
 
 /**
@@ -39,14 +42,30 @@ public final class PrescriptionRequestServlet extends AbstractDatabaseServlet {
         String patient_cf = (String) session.getAttribute("cf");
 
         // request parameters
-        String doctor_cf = null;
+        String doctor_email;
+
         String description = null;
+
         String type = null;
 
+        int duration = 0;
+
+        /*
+            identifier of exam or medicine, depends on the value of type
+         */
+        String code = null;
+
+        int quantity = 0;
+
+
         //other prescription info
-        Date date = null;  //should be use the request date, format: yyyy-mm-dd
-        Prescription.Status status = Prescription.Status.PENDING; //must be set as pending, we are just instantiate the request
-        int duration = 0; //the doctor will change this parameter when he accept the request
+        String id = null;
+
+        String doctor_cf = null;
+
+        Date date = null;
+
+        Prescription.Status status = Prescription.Status.PENDING;
 
         // model
         Prescription prescription = null;
@@ -54,43 +73,56 @@ public final class PrescriptionRequestServlet extends AbstractDatabaseServlet {
 
         try{
             // retrieves the request parameters
-            doctor_cf = req.getParameter("doctor cf");  //todo: invece di chiedere il cf del dottore (come fa a saperlo scrive nome e cognome e lo cerco io il cf
+            doctor_email = req.getParameter("doctor_email");
             description = req.getParameter("description");
+            duration = Integer.parseInt(req.getParameter("numeroprestazioni"));
+            date = new Date(System.currentTimeMillis());
+            code = req.getParameter("code");
+            quantity = Integer.parseInt(req.getParameter("qnt"));
+
+            if(duration<1 || duration>10){
+                throw new InputFormatException("Validity times must be greater than 0 and lower than 11!");
+            }
+            if(quantity<1 || quantity>100){
+                throw  new InputFormatException("The quantity must be in the range of 1-100");
+            }
+
             type = req.getParameter("type");
 
             // Check if some parameters are null
-            if(doctor_cf == null || patient_cf == null || description == null || type == null ) {
+            if(doctor_email == null || patient_cf == null || description == null || type == null ) {
                 throw new InputFormatException("One or more input parameters are null");
             }
 
             // Check if some parameters are empty
-            if(doctor_cf.isEmpty() || patient_cf.isEmpty() || description.isEmpty() || type.isEmpty() ) {
+            if(doctor_email.isEmpty() || patient_cf.isEmpty() || description.isEmpty() || type.isEmpty() ) {
                 throw new InputFormatException("One or more input parameters are empty");
             }
 
             // Removing leading and trailing white space
-            doctor_cf = doctor_cf.trim().toUpperCase();
+            doctor_email = doctor_email.trim();
             patient_cf = patient_cf.trim().toUpperCase();
             description = description.trim();
+            code = code.trim().toUpperCase();
 
             // Check if cf has a bad format
-            if(doctor_cf.length() != 16 || doctor_cf.contains(" ")){
+            if(doctor_email.contains(" ")){
                 throw new InputFormatException("The format of the parameter Codice Fiscale is wrong");
             }
             if(patient_cf.length() != 16 || patient_cf.contains(" ")){
                 throw new InputFormatException("The format of the parameter Codice Fiscale is wrong");
             }
 
-            date = new Date(System.currentTimeMillis());
+
 
             // Create a new Gender enum with the gender type retrieved from the form
             Prescription.TypeOfPrescription typeEnum = Prescription.TypeOfPrescription.valueOf(type);
 
             // creates a new patient from the request parameters
-            prescription = new Prescription(doctor_cf, patient_cf, date, description, duration, typeEnum, status);
+            prescription = new Prescription(id, doctor_cf, patient_cf, date, description, duration, typeEnum, status);
 
             // creates a new object for accessing the database and stores the patient
-            new PrescriptionRequestDAO(getDataSource().getConnection(), prescription).prescriptionRequest();
+            new PrescriptionRequestDAO(getDataSource().getConnection(), prescription, doctor_email, code, quantity).prescriptionRequest();
 
             message = new Message("Prescription successfully forwarded to the doctor.");
 
@@ -120,6 +152,23 @@ public final class PrescriptionRequestServlet extends AbstractDatabaseServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         HttpSession session = req.getSession(false);
         String patient_cf = (String) session.getAttribute("cf");
+
+        List<Medicine> medicineList = null;
+        Message message = null;
+
+        try{
+            // creates a new object for accessing the database and stores the patient
+            ListMedicinesDAO show = new ListMedicinesDAO(getDataSource().getConnection());
+            medicineList = show.getListMedicines();
+            message = new Message("Medicine succesfully searched!");
+
+        } catch (SQLException ex) {
+            message = new Message("Cannot search for employees: unexpected error while accessing the database.",
+                    "E200", ex.getMessage());
+        }
+
+        req.setAttribute("medicineList", medicineList);
+        req.setAttribute("message", message);
         req.setAttribute("cf", patient_cf);
 
         // forwards the control

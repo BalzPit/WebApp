0b30e19d16a61a035b3364f0934216b8cbb4991b
manuel <manuel.bettin@studenti.unipd.it>
2021-04-18 01:11:18 +0200

    updated

--- 4doctors/src/main/database/PopolateDbMed-v1.sql
+++ 4doctors/src/main/database/PopolateDbMed-v1.sql
@@ -21,6 +21,9 @@ INSERT INTO doctors.Patologia VALUES ('53A', 'tonsillite', 'A', null);
 
 INSERT INTO doctors.Farmaco VALUES ('1322AB0001', 'Tachipirina', 'OTC', 'Angelini', 'gocce');
 INSERT INTO doctors.Farmaco VALUES ('234DF21345', 'Broncovaleas', 'SOP', 'Valeant', 'compresse 100mg');
+INSERT INTO doctors.Farmaco VALUES ('JS7EH284HD', 'Acamba', 'ETICI', 'Sooft Italia S.p.a.', 'Antiglaucoma');
+INSERT INTO doctors.Farmaco VALUES ('H2E7G45KN9', 'Cabaser', 'ETICI', 'Pfizer', 'Antiparkinson, inibitori della prolattina');
+INSERT INTO doctors.Farmaco VALUES ('W5EGH729KF', 'Tachidol', 'ETICI', 'Angelini', 'Antipiretici, analgesici FANS');
 
 INSERT INTO doctors.PrincipioAttivo VALUES ('AC14', 'Salbutamolo');
 INSERT INTO doctors.PrincipioAttivo VALUES ('B4C1', 'Paracetamolo');
@@ -28,6 +31,29 @@ INSERT INTO doctors.PrincipioAttivo VALUES ('B4C1', 'Paracetamolo');
 INSERT INTO doctors.Esame VALUES ('H01', 'Esame del sangue');
 INSERT INTO doctors.Esame VALUES ('CB2', 'Radiografia ginocchio dx');
 INSERT INTO doctors.Esame VALUES ('AC23', 'Gastroscopia');
+insert into doctors.esame values ('END1', 'Laringoscopia');
+insert into doctors.esame values ('END2', 'Broncoscopia');
+insert into doctors.esame values ('END3', 'Esofagoscopia');
+insert into doctors.esame values ('END4', 'Gastroscopia');
+insert into doctors.esame values ('END5', 'Endroscopia gastroduodenale');
+insert into doctors.esame values ('END6', 'Colonscopia');
+insert into doctors.esame values ('END7', 'Citoscopia');
+insert into doctors.esame values ('END8', 'Isteroscopia');
+insert into doctors.esame values ('END9', 'Artroscopia');
+insert into doctors.esame values ('END10', 'Laparoscopia');
+insert into doctors.esame values ('END11', 'Mediastinoscopia');
+insert into doctors.esame values ('END12', 'Torascopia');
+insert into doctors.esame values ('ECG1', 'Elettrocardiografia');
+insert into doctors.esame values ('EEG', 'Elettroencefalografia');
+insert into doctors.esame values ('BPS1', 'Biopsia');
+insert into doctors.esame values ('RADGF1', 'Radiografia');
+insert into doctors.esame values ('ECOGF1', 'Ecografia');
+insert into doctors.esame values ('SCIGF1', 'Scintigrafia');
+insert into doctors.esame values ('TC1', 'Tomografia Computerizzata');
+insert into doctors.esame values ('RMI1', 'Risonanza magnetica immagini');
+insert into doctors.esame values ('PET1', 'Tomografia emissione positroni');
+insert into doctors.esame values ('ANGGF1', 'Angiografia');
+
 
 INSERT INTO doctors.Ricetta VALUES (176429611, 'F345MED1K0283759', 'MLNMTT96E16H816G', '2018-04-21', 'Gastroscopia per certificazione intolleranze', 1, '80', 'URG');
 INSERT INTO doctors.Ricetta VALUES (283723789, 'SON01MED1C0G1UR0', 'MTTRSO97E67L781I', '2019-05-09', 'Tachipirina gocce', 3, '11', null );

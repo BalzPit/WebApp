ba1b43f965d2b65f39f827602219a7111f16b740
manuel <manuel.bettin@studenti.unipd.it>
2021-04-20 00:19:25 +0200

    add two methods for an eventual future rest

--- 4doctors/src/main/java/it/unipd/dei/webapp/resource/Prescription.java
+++ 4doctors/src/main/java/it/unipd/dei/webapp/resource/Prescription.java
@@ -1,9 +1,20 @@
 package it.unipd.dei.webapp.resource;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 // Represents the data about a presecription
-public class Prescription {
+public class Prescription extends Resource {
+
+
 
     public enum Status {
         PENDING, APPROVED, REJECTED
@@ -139,6 +150,119 @@ public class Prescription {
      */
     public final Status getStatus() {
         return status;
+    }
+
+    @Override
+    public final void toJSON(OutputStream out) throws IOException {
+        final JsonGenerator jg = JSON_FACTORY.createGenerator(out);
+        jg.writeStartObject();
+
+        jg.writeFieldName("prescription");
+
+        jg.writeStartObject();
+
+        jg.writeStringField("id", id);
+
+        jg.writeStringField("doctor", doctor);
+
+        jg.writeStringField("patient", patient);
+
+        jg.writeStringField("date", date.toString()); //date?? can treat it as a date?
+
+        jg.writeStringField("description", description);
+
+        jg.writeNumberField("duration", duration);
+
+        jg.writeStringField("type", type.name());
+
+        jg.writeStringField("status", status.name());
+
+        jg.writeEndObject();
+
+        jg.writeEndObject();
+
+        jg.flush();
+    }
+
+    /**
+     * Creates a {@code Medicine} from its JSON representation.
+     *
+     * @param in the input stream containing the JSON document.
+     *
+     * @return the {@code Medicne} created from the JSON representation.
+     *
+     * @throws IOException if something goes wrong while parsing.
+     */
+    public static Prescription fromJSON(final InputStream in) throws IOException, ParseException {
+
+        // the fields read from JSON
+        String jId = null;
+        String jDoctor = null;
+        String jPatient = null;
+        Date jDate = null;
+        String jDescription = null;
+        int jDuration = -1;
+        TypeOfPrescription jType = null;
+        Status jStatus = null;
+
+        String jDateS = null; //for getting the date from JSON object
+
+        final JsonParser jp = JSON_FACTORY.createParser(in);
+
+        // while we are not on the start of an element or the element is not
+        // a token element, advance to the next element (if any)
+        while (jp.getCurrentToken() != JsonToken.FIELD_NAME || "prescription".equals(jp.getCurrentName()) == false) {
+
+            // there are no more events
+            if (jp.nextToken() == null) {
+                throw new IOException("Unable to parse JSON: no prescription object found.");
+            }
+        }
+
+        while (jp.nextToken() != JsonToken.END_OBJECT) {
+
+            if (jp.getCurrentToken() == JsonToken.FIELD_NAME) {
+
+                switch (jp.getCurrentName()) {
+                    case "id":
+                        jp.nextToken();
+                        jId = jp.getText();
+                        break;
+                    case "doctor":
+                        jp.nextToken();
+                        jDoctor = jp.getText();
+                        break;
+                    case "patient":
+                        jp.nextToken();
+                        jPatient = jp.getText();
+                        break;
+                    case "date":
+                        jp.nextToken();
+                        jDateS = jp.getText();
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH");
+                        jDate = (Date) sdf.parse(jDateS);
+                        break;
+                    case "description":
+                        jp.nextToken();
+                        jDescription = jp.getText();
+                        break;
+                    case "duration":
+                        jp.nextToken();
+                        jDuration = jp.getIntValue();
+                        break;
+                    case "type":
+                        jp.nextToken();
+                        jType = Prescription.TypeOfPrescription.valueOf(jp.getText());
+                        break;
+                    case "status":
+                        jp.nextToken();
+                        jStatus = Prescription.Status.valueOf(jp.getText());
+                        break;
+                }
+            }
+        }
+
+        return new Prescription(jId, jDoctor, jPatient, jDate, jDescription, jDuration, jType, jStatus);
     }
 
 }
